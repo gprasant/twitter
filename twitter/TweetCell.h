e4bb1bef95d8d3439f668eaf32a976ab8ebc2592
Prasanth Guruprasad <nagu89@gmail.com>
2015-11-08 10:54:51 -0800

    add readme comments

---
+++ twitter/TweetCell.h
@@ -0,0 +1,13 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Prasanth Guruprasad on 11/8/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TweetCell : UITableViewCell
+//    start adding members
+@end
---
+++ twitter/TweetCell.m
@@ -0,0 +1,23 @@
+//
+//  TweetCell.m
+//  twitter
+//
+//  Created by Prasanth Guruprasad on 11/8/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import "TweetCell.h"
+
+@implementation TweetCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
